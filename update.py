25bd1d3e2d05a15bd50f90c1b610d58169778e36
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-11-30 13:00:49 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,20 +1,30 @@
-
+from os import path as opath, getenv
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+from logging.handlers import RotatingFileHandler
 from subprocess import run as srun
-import logging
-from os import path as ospath
+from dotenv import load_dotenv
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
-                    level=logging.INFO)
+if opath.exists("log.txt"):
+    with open("log.txt", 'r+') as f:
+        f.truncate(0)
 
-UPSTREAM_REPO = 'https://github.com/abhinai2244/Encoding-Bot.git'
-UPSTREAM_BRANCH = 'audioswap'
+basicConfig(format="[%(asctime)s] [%(name)s | %(levelname)s] - %(message)s [%(filename)s:%(lineno)d]",
+            datefmt="%m/%d/%Y, %H:%M:%S %p",
+            handlers=[FileHandler('log.txt'), StreamHandler()],
+            level=INFO)
+
+load_dotenv('config.env', override=True)
+
+UPSTREAM_REPO = getenv('UPSTREAM_REPO')
+UPSTREAM_BRANCH = getenv('UPSTREAM_BRANCH')
 
 if UPSTREAM_REPO is not None:
-    if ospath.exists('.git'):
+    if opath.exists('.git'):
         srun(["rm", "-rf", ".git"])
 
     update = srun([f"git init -q \
+                     && git config --global user.email drxxstrange@gmail.com \
+                     && git config --global user.name SilentDemonSD \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
@@ -22,6 +32,6 @@ if UPSTREAM_REPO is not None:
                      && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
     if update.returncode == 0:
-        logging.info('Successfully updated with latest commit from UPSTREAM_REPO')
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
     else:
-        logging.error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
